e7046981c210c136750446ca70e5c8b354f728f2
rimkomatic <mahapatrarik123@gmail.com>
2026-06-21 18:01:14 +0530

    fixed places

--- common/.config/custom/.config/custom/settings/ai.sh
+++
@@ -1,1 +0,0 @@
-$(cat ~/.config/custom/settings/browser.sh) --new-window https://chat.openai.com
--- common/.config/niri/.config/niri/scripts/clipboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sleep 1
-wl-paste --type text --watch cliphist store &
-wl-paste --type image --watch cliphist store &
-wait
